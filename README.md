9f03b60f6833eebe38dbec0954b6ed5f440a620b
Alvian Z F <alvianzf@gmail.com>
2023-11-14 22:58:54 +0700

    chore: refactor code

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
-# Chrome extension project
+# Chrome Extension Project
 
-### How to install
-- Clone the project from the repository using `git clone`.  
-- On your google chrome browser, navigate to `chrome://extensions/`.  
-- Click on `Load unpacked` button on the top right corner.  
-- Find the folder of the project you have cloned.  
-- Load the extension by navigating to `https://web.whatsapp.com/`
+This repository contains the code for a Chrome extension project. The extension enhances the user experience on the `https://web.whatsapp.com/` page.
 
----
+## Installation
 
+Follow the steps below to install the extension:
 
-Author: Alvian Zachry Faturrahman
+1. Clone the project from the repository using `git clone`.
+
+2. Open your Google Chrome browser and navigate to `chrome://extensions/`.
+
+3. Enable Developer mode by clicking the toggle switch at the top right corner.
+
+4. Click on the `Load unpacked` button on the top left corner.
+
+5. Navigate to the folder of the project you have cloned and select it.
+
+6. The extension is now loaded and ready to use on `https://web.whatsapp.com/`.
+
+## Usage
+
+After installation, navigate to `https://web.whatsapp.com/` to start using the extension. The extension will automatically apply its enhancements to the page.
+
+## Author
+
+Alvian Zachry Faturrahman
--- content-script.js
+++ content-script.js
@@ -1,108 +1,118 @@
-const css = `<style>
-.top-bar {
-    width: 100vw;
-    height: 4em;
-    font-size: large;
-    display: flex;
-    justify-content: space-between;
-    position: fixed;
-    top: 0;
-    left: 0;
-    background: white;
-    z-index: 999;
-    box-sizing: border-box;
-    padding: 1em;
-    color: black !important;
-}
 
-.side-bar {
-    width: 20em;
-    height: 100vh;
-    font-size: large;
-    display: none;
-    position: fixed;
-    top: 4em;
-    right: 0;
-    background: white;
-    z-index: 999;
-    box-sizing: border-box;
-    padding: 1em;
-    color: black !important;
-}
-
-.toggle-button {
-    padding: 0.1em 1em;
-    color: white;
-    background: blue;
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 20px;
-}
-
-.toggle-button:hover {
-    background: darkblue;
-}
-
-#app {
-    top: 4em !important;
-    height: calc(100% - 6em) !important;
-}
-
-</style>`;
-
-const topBar = `<div class="top-bar">
-    Lorem ipsum dolor sit amet
-    <div id="toggle-button" class="toggle-button">Show</div>
-</div>`;
-
-var toggle = false;
-
-function toggleSidebar() {
-    toggle = !toggle;
-
-    // Get the sideBar and app elements
-    let sideBar = document.querySelector('.side-bar');
-    let app = document.getElementById('app');
-    let btn = document.getElementById('toggle-button')
-
-    // If toggle is false, hide the sideBar and set the #app width to 100%
-    if (!toggle) {
-        sideBar.style.display = 'none';
-        app.style.width = '100%';
-        btn.innerHTML = 'Show'
+class App {
+    constructor() {
+        this.toggle = false;
+        this.app = document.getElementById('app');
+        this.observer = this.createObserver();
     }
-    // If toggle is true, show the sideBar and set the #app width to calc(100vw - 22em)
-    else {
-        sideBar.style.display = 'flex';
-        app.style.width = 'calc(100vw - 23em)';
-        btn.innerHTML = 'Hide'
+
+    createStyles() {
+        const styleElement = document.createElement('style');
+        styleElement.innerHTML = `
+        .top-bar {
+            width: 100vw;
+            height: 4em;
+            font-size: large;
+            display: flex;
+            justify-content: space-between;
+            position: fixed;
+            top: 0;
+            left: 0;
+            background: white;
+            z-index: 999;
+            box-sizing: border-box;
+            padding: 1em;
+            color: black !important;
+        }
+        
+        .side-bar {
+            width: 20em;
+            height: 100vh;
+            font-size: large;
+            display: none;
+            position: fixed;
+            top: 4em;
+            right: 0;
+            background: white;
+            z-index: 999;
+            box-sizing: border-box;
+            padding: 1em;
+            color: black !important;
+        }
+        
+        .toggle-button {
+            padding: 0.1em 1em;
+            color: white;
+            background: blue;
+            cursor: pointer;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 20px;
+        }
+        
+        .toggle-button:hover {
+            background: darkblue;
+        }
+        
+        #app {
+            top: 4em !important;
+            height: calc(100% - 6em) !important;
+        }
+        `;
+        return styleElement;
+    }
+
+    createTopBar() {
+        const topBar = document.createElement('div');
+        topBar.className = 'top-bar';
+        topBar.innerHTML = `
+            Lorem ipsum dolor sit amet
+            <div id="toggle-button" class="toggle-button">Show</div>
+        `;
+        return topBar;
+    }
+
+    createSideBar() {
+        const sideBar = document.createElement('div');
+        sideBar.className = 'side-bar';
+        sideBar.textContent = 'SideBar';
+        return sideBar;
+    }
+
+    toggleSidebar() {
+        this.toggle = !this.toggle;
+        const sideBar = document.querySelector('.side-bar');
+        const app = document.getElementById('app');
+        const btn = document.getElementById('toggle-button');
+        if (!this.toggle) {
+            sideBar.style.display = 'none';
+            app.style.width = '100%';
+            btn.textContent = 'Show';
+        } else {
+            sideBar.style.display = 'flex';
+            app.style.width = 'calc(100vw - 23em)';
+            btn.textContent = 'Hide';
+        }
+    }
+
+    createObserver() {
+        return new MutationObserver((mutationsList, observer) => {
+            if (this.app.childElementCount > 0) {
+                document.body.prepend(this.createStyles());
+                document.body.prepend(this.createTopBar());
+                document.body.prepend(this.createSideBar());
+                document.getElementById('toggle-button').addEventListener('click', () => this.toggleSidebar());
+                observer.disconnect();
+            }
+        });
+    }
+
+    start() {
+        this.observer.observe(this.app, { childList: true });
     }
 }
 
-const sideBar = `<div class="side-bar">
-SideBar </div>`;
+const app = new App();
+app.start();
 
-// Get the app element
-let app = document.getElementById('app');
-
-// Create a new MutationObserver instance
-let observer = new MutationObserver((mutationsList, observer) => {
- // If the app element has at least one child element
- if (app.childElementCount > 0) {
-   // Insert the CSS
-   document.body.insertAdjacentHTML('afterbegin', css);
-   document.body.insertAdjacentHTML('afterbegin', topBar);
-   document.body.insertAdjacentHTML('afterbegin', sideBar);
-
-   // Disconnect the observer
-   observer.disconnect();
-
-   // Add event listener to the toggle button
-   document.getElementById('toggle-button').addEventListener('click', toggleSidebar);
- }
-});
-
-// Start observing the app element for childList changes
-observer.observe(app, { childList: true });
